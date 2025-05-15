3fd260a0adb3a31d8e51f04d2f9b36701d39be86
sarahbarreto <sarahbarretopublictaria@gmail.com>
2025-05-14 21:32:06 -0300

    atualizacao

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask (__name__)
+
+@app.route('/')
+def ola ():
+    # return '<h1> Olá, Mundo! </h1>'
+    return render_template('index.html')
+
+@app.route('/sobre-equipe')
+def sobre_equipe():
+    # return '<h1> Olá, Mundo! </h1>'
+    return render_template('sobre.html')
+
+
+
+app.run()
+
